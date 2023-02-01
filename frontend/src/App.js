c2df65f545a7faba6646ba21ac222f14b5c26177
lynchu <lyn15chu@gmail.com>
2023-02-01 15:35:19 +0800

    add signin and signup pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 import Home from './components/Home';
 import Projects from './components/Projects';
 import About from './components/About';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Link } from "react-router-dom";
 import NavBar from './NavBar';
-import Map from './components/SimpleMap'
-
+import Map from './components/SimpleMap';
+import Login from './components/Login/login';
 
 function App() {
   return (
     <div className="App">
-        <NavBar />
-        <Routes>
-            <Route exact path="/" element={<Map />} />
-            <Route exact path="/about" element={<About />} />
-            <Route exact path="/projects" element={<Projects />} />
-        </Routes>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<Map />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/projects" element={<Projects />} />
+        <Route exact path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
--- frontend/src/NavBar.js
+++ frontend/src/NavBar.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-function NavBar(){
-    return(
+function NavBar() {
+    return (
         <div align="right">
             <button type="button" class="btn btn-link"><Link to="/">Home</Link></button>
             <button type="button" class="btn btn-link"><Link to="/about">About</Link></button>
             <button type="button" class="btn btn-link"><Link to="/projects">Projects</Link></button>
+            <button type="button" class="btn btn-link"><Link to="/login">Login</Link></button>
         </div>
     );
 }
